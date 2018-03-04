814d6827b0ec2a69e16a1d5ad5693915a00a6aa9
Vladimir <cadaver@tut.by>
2018-03-04 13:09:37 +0300

    menu

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -5,6 +5,7 @@ namespace Corp\Http\Controllers;
 use Corp\Repositories\MenusRepository;
 use Illuminate\Http\Request;
 use Corp\Http\Requests;
+use Menu;
 
 
 class SiteController extends Controller
@@ -28,15 +29,29 @@ class SiteController extends Controller
     protected function renderOutput()
     {
         $menu = $this->getMenu();
-        $navigation = view(env('THEME').'.navigation')->render();
-        $this->vars = array_add($this->vars,'navigation', $navigation);
+        $navigation = view(env('THEME') . '.navigation')->with('menu', $menu)->render();
+        $this->vars = array_add($this->vars, 'navigation', $navigation);
         return view($this->template)->with($this->vars);
     }
 
     protected function getMenu()
     {
         $menu = $this->m_rep->get();
-        return $menu;
+
+        $mBuilder = Menu::make('MyNav', function ($m) use ($menu) {
+            foreach ($menu as $item) {
+                if ($item->parent == 0) {
+                    $m->add($item->title, $item->path)->id($item->id);
+                } else {
+                    if ($m->find($item->parent)) {
+                        $m->find($item->parent)->add($item->title, $item->path)->id($item->id);
+                    }
+                }
+            }
+        });
+        //dd($mBuilder);
+
+        return $mBuilder;
     }
 
 
